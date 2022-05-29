4f9b25800aaa772755ca7cf84432029263e5f23e
Tulio Calil <tuliocalil@Air-de-Tulio.lan>
2022-05-29 10:02:04 -0300

    fix images href

--- src/components/markdown/renderers/img.svelte
+++ src/components/markdown/renderers/img.svelte
@@ -2,6 +2,25 @@
   export let href = "";
   export let title = undefined;
   export let text = "";
+
+  let url = "";
+
+  const check = checkUrl(href);
+
+  if (check) {
+    url = href;
+  } else {
+    url = `${API_URL}${href}`;
+  }
+
+  function checkUrl(url) {
+    try {
+      new URL(url);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
 </script>
 
-<img src={`${API_URL}${href}`} {title} alt={text} />
+<img src={url} {title} alt={text} />
