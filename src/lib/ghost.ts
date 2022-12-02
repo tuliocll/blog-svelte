00a3bbca78a62f29252d7532bfd787931047cdc1
Tulio Calil <tuliocalil@MacBook-Air-de-Tulio.local>
2022-12-01 22:08:21 -0300

    remove local key

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,8 +1,8 @@
 import GhostContentAPI from '@tryghost/content-api';
 
 const api = new GhostContentAPI({
-	url: 'http://localhost:2368',
-	key: '4f1a6bf1a83634b5e65c786fa9',
+	url: import.meta.env ? import.meta.env.VITE_GHOST_URL : '',
+	key: import.meta.env ? import.meta.env.VITE_GHOST_KEY : '',
 	version: 'v5.0'
 });
 
