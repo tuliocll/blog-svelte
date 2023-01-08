5b97239e005bb6861b62d3ea5aa8d47851aabf53
Tulio Calil <tuliocll@gmail.com>
2023-01-08 00:05:58 +0000

    chore: new amazon provider

---
+++ src/components/markdown/renderers/amazon.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { LazyImage } from 'svelte-lazy-image';
+
+	export let href = '';
+	export let title = undefined;
+	export let text = '';
+
+	let url = '';
+
+	let API_URL = import.meta.env ? import.meta.env.VITE_API_URL : '';
+
+	const check = checkUrl(href);
+
+	if (check) {
+		url = href;
+	} else {
+		url = `${API_URL}${href}`;
+	}
+
+	function checkUrl(url) {
+		try {
+			new URL(url);
+			return true;
+		} catch (_) {
+			return false;
+		}
+	}
+</script>
+
+=<h1>Work</h1>
