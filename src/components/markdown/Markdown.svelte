5aa8d5cd12380d68b28969e49f22f246edd8a629
Tulio Calil <tuliocalil@MacBook-Air-de-Tulio.local>
2023-01-08 21:38:15 -0300

    clean files

--- src/components/markdown/Markdown.svelte
+++ src/components/markdown/Markdown.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import SvelteMarkdown from 'svelte-markdown';
 	import ImageComponent from './renderers/img.svelte';
-	import AmazonComponent from './renderers/amazon.svelte';
-	import { onMount } from 'svelte';
 	export let content: string;
-
-	
 </script>
-
 
 <SvelteMarkdown source={content ?? ''} renderers={{ image: ImageComponent }} />
